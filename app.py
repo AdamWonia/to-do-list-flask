943f9325e319362cf8523c7b9c67c2a9f3876571
Adam <adam.wonia@gmail.com>
2021-11-21 20:46:38 +0100

    Task model added, database sqlite created

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class ToDoList(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    task_content = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    def __str__(self):
+        return f'Task {self.id}'
 
 
 @app.route('/tasks', methods=['GET', 'POST'])
